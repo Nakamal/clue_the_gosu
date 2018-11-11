e1230ae7c5fe82c414c26bd76efeeaca75404f79
Nakamal <nickhaugland@gmail.com>
2018-11-10 22:49:51 -0600

    THIS IS THE VERSION TO COME BACK TO IF YOU FUCKED UP THE MOVING ON BOARD LOGIC cleared out all the past participations, deckings, games and sheet_infos, it runnin lean now boy

--- models/die.rb
+++ models/die.rb
@@ -1,34 +1,32 @@
-require 'gosu'
+# require 'gosu'
 
-class Die
-  attr_accessor :sides, :value, :finished
+# class Die
+#   attr_accessor :sides, :value, :finished
 
-  def initialize(options_hash)
-    @sides = options_hash[:sides]
-    @value = 1
-    @x = options_hash[:x]
-    @y = options_hash[:y]
-    @radius = 30
-    @images = Gosu::Image.load_tiles('images/explosions.png', 60, 60) #
-    @image_index = 0
-    @finished = false
-  end
+#   def initialize(options_hash)
+#     @sides = options_hash[:sides]
+#     @value = 1
+#     @x = options_hash[:x]
+#     @y = options_hash[:y]
+#     @radius = 30
+#     @images = Gosu::Image.load_tiles('images/explosions.png', 60, 60) #
+#     @image_index = 0
+#     @finished = false
+#   end
 
-  def roll
-    @value = rand(1..sides)
-    @total = @value
-  end
-end
+#   def roll
+#     @value = rand(1..sides)
+#     @total = @value
+#   end
+# end
 
-def draw
-  if @image_index < @images.count
-    @images[@image_index].draw(@x - @radius, @y - @radius, 2)
-    @image_index += 1
-  else
-    @finished = true
-  end
-end
+# def draw
+#   if @image_index < @images.count
+#     @images[@image_index].draw(@x - @radius, @y - @radius, 2)
+#     @image_index += 1
+#   else
+#     @finished = true
+#   end
+# end
 
-d6 = Die.new(sides: 6)
-
-p d6.roll
+# d6 = Die.new(sides: 6)
